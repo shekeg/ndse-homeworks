8b15a8ecac9dcc3bfd91cae72ea248d3fdf9668c
Yievgeniy Shek <shek@chocotravel.com>
2021-04-10 21:08:58 +0600

    chore(007-middleware): add error middlewares

--- 007-middleware/index.js
+++ 007-middleware/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 
 const { userRouter, booksRouter } = require('./routers/index');
+const { notFoundMiddleware, errorHandleMiddleware } = require('./middlewares/errors');
 
 const app = express();
 app.use(express.json());
 
 app.use('/api/user', userRouter);
 app.use('/api/books', booksRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandleMiddleware);
 
 app.listen(3000);
---
+++ 007-middleware/middlewares/errors.js
@@ -0,0 +1,15 @@
+function notFoundMiddleware(req, res) {
+  res.status(404);
+  res.json({ error: 'Not found' });
+}
+
+// eslint-disable-next-line no-unused-vars
+function errorHandleMiddleware(err, req, res, next) {
+  res.status(500);
+  res.json({ error: err.message });
+}
+
+module.exports = {
+  notFoundMiddleware,
+  errorHandleMiddleware,
+};
