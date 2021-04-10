0b470529ce7a5ef36643570b3f1acae44328c801
Yievgeniy Shek <shek@chocotravel.com>
2021-04-11 00:40:02 +0600

    feat(008-ejs): add main site router

--- 008-ejs/index.js
+++ 008-ejs/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
-const { stieBooksRouter, apiUserRouter, apiBooksRouter } = require('./routers/index');
+const {
+  siteMainRouter, stieBooksRouter, apiUserRouter, apiBooksRouter,
+} = require('./routers/index');
 const { notFoundMiddleware, errorHandleMiddleware } = require('./middlewares/errors');
 
 const app = express();
@@ -10,6 +12,7 @@ app.set('view engine', 'ejs');
 
 app.use('/public', express.static(`${__dirname}/public`));
 
+app.use('/', siteMainRouter);
 app.use('/books', stieBooksRouter);
 
 app.use('/api/user', apiUserRouter);
--- 008-ejs/routers/index.js
+++ 008-ejs/routers/index.js
@@ -1,9 +1,11 @@
+const { siteMainRouter } = require('./site/main-router');
 const { stieBooksRouter } = require('./site/books-router');
 
 const { apiUserRouter } = require('./api/user-router');
 const { apiBooksRouter } = require('./api/books-router');
 
 module.exports = {
+  siteMainRouter,
   stieBooksRouter,
   apiUserRouter,
   apiBooksRouter,
