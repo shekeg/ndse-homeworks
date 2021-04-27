8345b2b789cdb0ae5dfa58c208aa80232cd9e7c9
Yievgeniy Shek <shek@chocotravel.com>
2021-04-28 00:43:40 +0600

    fix(011-mongo): set  error Code: 404 message if book wasn't find

--- 011-mongo/library-app/src/db/books-db.js
+++ 011-mongo/library-app/src/db/books-db.js
@@ -17,15 +17,28 @@ const booksDb = {
     return BookModel.find();
   },
   findById({ id }) {
-    // eslint-disable-next-line no-underscore-dangle
-    return BookModel.findById(id).exec().then((res) => res._doc);
+    return BookModel.findById(id).exec()
+      .then((res) => {
+        if (res === null) {
+          return Promise.reject(new Error('Code: 404'));
+        }
+        // eslint-disable-next-line no-underscore-dangle
+        return res._doc;
+      });
   },
   insert({ id, ...bookInfo }) {
     const book = new BookModel(bookInfo);
     return book.save();
   },
   update({ id, ...bookInfo }) {
-    return BookModel.findByIdAndUpdate(id, bookInfo, { new: true });
+    return BookModel.findByIdAndUpdate(id, bookInfo, { new: true })
+      .then((res) => {
+        if (res === null) {
+          return Promise.reject(new Error('Code: 404'));
+        }
+        // eslint-disable-next-line no-underscore-dangle
+        return res;
+      });
   },
   remove({ id }) {
     return BookModel.deleteOne({ _id: id });
